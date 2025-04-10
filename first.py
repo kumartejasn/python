90b10b7d3ebd8a2f00fe6ba76676c5662afc27ae
tejas <tejaskumarcs20@gmail.com>
2025-04-10 21:22:09 +0530

    Update first.py with latest changes

--- first.py
+++ first.py
@@ -1,2 +1,35 @@
 print("Hello, world!")
-print("my age is",20)
+
+
+name="tejas" #String
+Name="kumar" #string
+# python is case sensitive
+
+age=24
+price=99.99 #Float
+
+old=True
+a=None
+print("my name is", name)
+print("my name is", Name)
+print("my age is", age)
+print(old)
+print(a)
+
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
+
+
+
+# FIRST PROBLEM
+
+num1=2
+num2=5
+res=num1+num2
+
+print(num1 + num2)
+print(res)
